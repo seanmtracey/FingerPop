f06adc34c3cf7c1104f25dc6ed4879a3a6e5e60e
Ralph Saunders <e@ralphsaunders.co.uk>
2012-05-07 19:35:39 +0100

    Balls now removed from active array when off-screen

--- scripts/core.js
+++ scripts/core.js
@@ -61,7 +61,7 @@ var particleArray = [];
 var Particle = function (x, y, radius, r, g, b, a, vx, vy){
 	this.x = x;
 	this.y = y;
-	this.radius = radius; 
+	this.radius = radius;
 	this.r = r;
 	this.g = g;
 	this.b = b;
@@ -75,42 +75,62 @@ var Particle = function (x, y, radius, r, g, b, a, vx, vy){
 animate();
 
 function animate(){
-	
+
 	var randomRadius = Math.floor(Math.random()*30);
-	
+
 	var randomRed = Math.floor(Math.random()*255);
 	var randomGreen = Math.floor(Math.random()*255);
 	var randomBlue = Math.floor(Math.random()*255);
-	
+
 	var randomVX = Math.random()*10 + Math.random()*-10 ;
 	var randomVY = Math.random()*10 + Math.random()*-10;
-	
+
 	if(makeParticles){
 		particleArray.push(new Particle(touchX, touchY, randomRadius, randomRed, randomGreen, randomBlue, 1, randomVX, randomVY));
 	}
-	
+
 	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-	
+
 	for(i=0;i<particleArray.length;i++){
-	
-		var tmpBall = particleArray[i];
-		
-		ctx.fillStyle = "rgba(" + tmpBall.r + ", " + tmpBall.g + ", " + tmpBall.b + ", " + tmpBall.a + ")";
-		
-		ctx.beginPath();
-		ctx.arc(tmpBall.x, tmpBall.y, tmpBall.radius, 0, Math.PI*2, false);
-		ctx.closePath;
-		ctx.fill();
-		
-		tmpBall.x += tmpBall.vx;
-		tmpBall.y += tmpBall.vy;
+
+        if(outOfBounds(particleArray[i].x, particleArray[i].y, particleArray[i].radius)) {
+            particleArray.splice(i, 1);
+        } else {
+            var tmpBall = particleArray[i];
+
+            ctx.fillStyle = "rgba(" + tmpBall.r + ", " + tmpBall.g + ", " + tmpBall.b + ", " + tmpBall.a + ")";
+
+            ctx.beginPath();
+            ctx.arc(tmpBall.x, tmpBall.y, tmpBall.radius, 0, Math.PI*2, false);
+            ctx.closePath;
+            ctx.fill();
+
+            tmpBall.x += tmpBall.vx;
+            tmpBall.y += tmpBall.vy;
+        }
 	}
-	
+
 	if(particleArray.length >= howMany){
 		particleArray.shift();
 	}
-	
+
 	setTimeout(animate, 33);
-	
+
 }
 
+/**
+ * Out Of Bounds
+ *
+ * Returns true if coordinates of ball place it off-screen.
+ */
+function outOfBounds(x, y, diameter) {
+    if( x < 0 - diameter || x > $( window ).width() + diameter ) {
+        if( y < 0 - diameter || y > $( window ).height() + diameter ) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
